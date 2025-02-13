043b7c8d21e70761f3f3a7104e3d9b84d1bb99fe
Stanislav Timoshko <s4kkkk@mail.ru>
2025-02-13 18:55:32 +0300

    gpio драйвер для stm32: добавил хедер с описанием интерфейса

---
+++ firmware/include/drivers_init/gpio_stm32_init.h
@@ -0,0 +1,93 @@
+/**
+ * @file 
+ * @brief Необходимое для низкоуровневой инициализации драйвера
+ *
+ * Описанные тут функции и структуры данных используются на этапе
+ * низкоуровневой инициализации драйверов (создании и регистрации экзепляров
+ * struct device)
+ *
+ * @author Stanislav Timoshko <s4kkkk@mail.ru>
+ */
+
+#ifndef INCLUDE_DRIVERS_INIT_GPIO_STM32_INIT_H
+#define INCLUDE_DRIVERS_INIT_GPIO_STM32_INIT_H
+
+#include <device.h>
+#include <drivers/gpio.h>
+
+
+struct gpio_stm32_settings;
+
+/**
+ * @brief Внутренняя конфигурация драйвера
+ */
+struct gpio_stm32_config {
+        /* gpio_driver_config должен быть первым */
+        struct gpio_driver_config common;
+
+        /* Пользовательские настройки */
+        const struct gpio_stm32_settings* user_settings;
+
+        /* Базовый адрес регистров */
+        uint32_t* base_reg_address;
+};
+
+/**
+ * @brief Данные драйвера
+ */
+struct gpio_stm32_data {
+        uint32_t flag;
+};
+
+extern struct gpio_driver_api gpio_stm32_driver_api;
+
+/**
+ * @brief Структура, которую нужно заполнить перед инициализацией
+ * драйвера
+ *
+ * @note Данные из этой структуры постоянны, поэтому она может быть
+ * создана в пользовательском коде с квалификатором const
+ */
+struct gpio_stm32_settings {
+        /**
+         * Номер обслуживаемого порта. Нумерация соответствует обозначениям
+         * в reference manual.
+         * GPIOA: 0
+         * GPIOB: 1
+         * ...
+         */
+        uint8_t port;
+};
+
+/**
+ * @macro
+ * @brief Создание экземпляра драйвера. Создание структуры device
+ *
+ * @param name имя создаваемого экземпляра драйвера. По этому имени можно
+ * найти этот экземпляр в реестре
+ */
+#define DEVICE_GPIO_STM32_DEFINE(name, settings)                        \
+        static struct gpio_stm32_config gpio_stm32_cfg_## name = {      \
+                .user_settings = &settings,                             \
+        };                                                              \
+                                                                        \
+        static struct gpio_stm32_data gpio_stm32_data_## name;          \
+                                                                        \
+        static const struct device device_gpio_stm32_## name = {        \
+                .name = #name,                                          \
+                .config = &(gpio_stm32_cfg_## name),                    \
+                .api = &gpio_stm32_driver_api,                          \
+                .data &(gpio_stm32_data_## name)                        \
+        }                                                               \
+
+
+/**
+ * @brief Конфигурирование драйвера
+ *
+ * Данная функция выполняет начальную низкоуровневую инициализацию драйвера.
+ *
+ * @param dev Структура, хранящая все внутрение данные
+ */
+int gpio_stm32_init_driver(struct device* dev);
+
+#endif /* INCLUDE_DRIVERS_INIT_GPIO_STM32_INIT_H */
